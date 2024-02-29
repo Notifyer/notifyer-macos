58e33b840b59cf28bd4b2dc4e05b0b93ef7bf582
noel <humblewolfstudio@gmail.com>
2024-02-29 20:23:57 +0100

    Initial Commit

---
+++ Notifyer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Notifyer
+//
+//  Created by Noel Cabus on 29/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Notifyer/NotifyerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotifyerApp.swift
+//  Notifyer
+//
+//  Created by Noel Cabus on 29/2/24.
+//
+
+import SwiftUI
+
+@main
+struct NotifyerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
